a966b9c5501b5caf3198588309d830a8196a9688
primex-melvin <nogoy.melvin@primex.ventures>
2025-12-17 14:08:50 +0800

    Dashboard page for redirect link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from "next/link";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen">
+      <Link href="/dashboard" className="font-bold text-lg">
+        Click here to go to Dashboard
+      </Link>
+    </div>
+  );
+};
 
-export default page
+export default Page;
